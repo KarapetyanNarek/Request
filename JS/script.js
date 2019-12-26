b5431c97fb02b65afe6ac6e4f1f599ad16dec178
Narek Karapetyan <nareklord@gmail.com>
2019-12-26 09:53:51 +0400

    create JSON file & create JS variables

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+let inputDram = document.getElementById('dram'),
+    inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd'),
+    inputGel = document.getElementById('gel');
